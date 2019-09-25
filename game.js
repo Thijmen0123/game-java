6d56e2fa2b440dc4a7dbe768bd8ae6ca504717c9
thijmen0123 <99058137@mydavnci.nl>
2019-09-25 11:38:23 +0200

    commit 1

---
+++ game.js
@@ -0,0 +1,40 @@
+alert('je werk in de albert hein er komt iemand binnen lopen vlak voor sluit hij loopt rustig rond en verstopt zich totdat de winkel is gesloten hij heeft daar een bivak muts op en met pistool houd die iemand onder schot en wilt geld hebben je ziet het gebeuren je staat er niet naast ')
+
+var vraag1 = prompt("Wat doe je of je rent weg of je doet wat hij zegt")
+
+
+if (vraag1 == "doet wat hij zegt"){
+alert('je hebt de goede keuze gemaakt je word nu gebruikt door hem en hij wilt dat je de kassa opent')
+var vraag2 = prompt("Wat doe je je opent de kassa of je schreeuwt naar die meneer dat je dat niet gaat doen  ")
+}
+
+if (vraag2 == "opent de kassa"){
+	alert('je hebt de goede keuze gemaakt je opent de kassa en er zit niks in hij wilt dat je de andere kassa ook open maakt ')
+var vraag3 = prompt("wat doe je nu open je de kassa of werk je tegen en doet niks ")
+}
+
+if (vraag3 == "open de kassa"){
+alert('je hebt hem opengemaakt en hij pakt het geld en wilt meer hij zegt dat hij naar de kluis wilt maar jij hebt de sleutel daar niet voor dus je moet je manager zoeken die is ergens in de winkel want de deuren zijn dicht  ')
+ var vraag4 = prompt("wat doe je zoeken voor de manager of proberen weg te rennen ")
+}
+
+if (vraag4 == "zoeken naar de manager"){
+alert('je hebt de goede keuze gemaakt je gaat nu met hem rondjes lopen en kijken waar de manager is ')
+var vraag5 = prompt("Wat doe je probeer je weg te rennen tijdens het zoeken of help je mee met zoeken tot je hem vind ")
+}
+
+
+if (vraag5 == "helpen met zoeken "){
+alert('je hebt de goede keuze gemaakt je ben nog steeds aan het zoeken en hij vraagt waar is de kantine ')
+var vraag6 = prompt("Wat doe je leid je hem naar de kantine of neem je een omweg om te kijken of de achterdeur open is ") 
+}
+
+
+if (vraag6 == "leid hem naar de kantine"){
+alert('je hebt de goede keuze gemaakt je loopt naar de kantine en de deur zit op slot want de manager zit daar in en jij hoort dat hij belt met de politie maar de meneer die je vast heeft hoort dat niet ')
+var vraag7 = prompt("Wat doe je rondjes lopen en tijd rekken of zeg je tegen hem dat de politie word gebeld ") 
+}
+
+
+if (vraag7 == "tijd rekken "){
+alert('je hebt de goede keuze gemaakt en hij zegt loop voor me rustig handen omhoog en geen rare bewegingen en niet rennen want dat schiet ik je ');}
